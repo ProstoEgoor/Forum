495038927c9b5796e91138c27c604b527c7fdc12
ProstoEgor <prosto.egor2108@gmail.com>
2021-10-19 14:52:14 +0400

    Add CatalogTag

---
+++ ForumModel/CatalogQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForumModel {
+    public class CatalogQuestion {
+        List<Question> ListQuestion { get; } = new List<Question>();
+
+        CatalogTag catalogTag;
+
+        public CatalogQuestion(CatalogTag catalogTag) {
+            this.catalogTag = catalogTag;
+        }
+
+        public void AddQuestion(Question question) {
+            ListQuestion.Add(question);
+            catalogTag.UpdateTags(question.Tags);
+        }
+    }
+}
--- ForumModel/CatalogTag.cs
+++ ForumModel/CatalogTag.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace ForumModel {
     public class CatalogTag {
